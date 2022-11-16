0e773739255e7e6a7f6f3b5fa53141a626c96cb8
VitaliyTka <36275752+VitaliyTka@users.noreply.github.com>
2022-11-16 23:49:06 +0200

    MSC-15: implement factorial

--- MySchoolCalcServer/Controllers/CalcController.cs
+++ MySchoolCalcServer/Controllers/CalcController.cs
@@ -22,6 +22,10 @@ namespace MySchoolCalcServer.Controllers
             try
             {
                 request = request.Replace("|", "/");
+                if (request.Contains('!'))
+                {
+                    request = CalcFactorial(request);
+                }
                 object response = new DataTable().Compute(request, null);
                 if (response == null)
                     return new CalcResponse(string.Empty);
@@ -32,6 +36,63 @@ namespace MySchoolCalcServer.Controllers
             {
                 return new CalcResponse(":(");
             }
+        }
+        private string CalcFactorial(string request)
+        {
+            int countFactorial = request.ToCharArray().Count(c => c == '!');
+            for (int i = 0; i < countFactorial; i++)
+            {
+                int factorialStartIndex = request.IndexOf('!');
+                int factorialEndIndex = request.IndexOf('!');
+                if (factorialEndIndex == 0)
+                    throw new Exception("");
+
+                bool isLastItem = false;
+                while (!isLastItem)
+                {
+                    if (factorialStartIndex == 0)
+                    {
+                        isLastItem = true;
+                    }
+                    else
+                    {
+                        string preItem = request[factorialStartIndex - 1].ToString();
+                        if (IsEndOfParam(preItem))
+                        {
+                            isLastItem = true;
+                        }
+                        else
+                        {
+                            factorialStartIndex--;
+                        }
+                    }
+                }
+                string factorial = request.Substring(factorialStartIndex, factorialEndIndex - factorialStartIndex);
+                string rezFactorial = Factorial(Int32.Parse(factorial)).ToString();
+                request = request.Remove(factorialStartIndex, factorialEndIndex - factorialStartIndex + 1);
+                request = request.Insert(factorialStartIndex, rezFactorial);
+            }
+            return request;
+        }
+        private bool IsEndOfParam(string preItem)
+        {
+            if (preItem == "-" ||
+                preItem == "+" ||
+                preItem == "(" ||
+                preItem == ")" ||
+                preItem == "/" ||
+                preItem == "*" ||
+                preItem == "!")
+            {
+                return true;
+            }
+            return false;
+        }
+        private int Factorial(int n)
+        {
+            if (n == 1) return 1;
+
+            return n * Factorial(n - 1);
         }
 
     }
