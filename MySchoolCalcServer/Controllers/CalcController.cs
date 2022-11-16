59bd0c925f46e9e01712e7ed530988beed32fffa
VitaliyTka <36275752+VitaliyTka@users.noreply.github.com>
2022-11-16 20:05:21 +0200

    MSC-12: add math calc api for base operations

---
+++ MySchoolCalcServer/Controllers/CalcController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace MySchoolCalcServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalcController : ControllerBase
+    {
+        private readonly ILogger<CalcController> _logger;
+
+        public CalcController(ILogger<CalcController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("math/{request}")]
+        public Response Get(string request)
+        {
+            object response = new DataTable().Compute(request, null);
+            if (response == null)
+                return new Response(string.Empty);
+
+            return new Response(response.ToString());
+        }
+
+    }
+}
