6099e6261ab572adbe8e3d13f5fa908d599925f7
VitaliyTka <vtkp72@gmail.com>
2022-11-16 18:03:00 +0200

    MSC: add geometric formulas page

---
+++ MySchoolCalcClient/Data/GeometricFormulasParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace MySchoolCalcClient.Data;
+
+public class GeometricFormulasParameters
+{
+    public string Path { get; set; }
+    public string Text { get; set; }
+
+    public GeometricFormulasParameters(string path, string text)
+    {
+        Path = path;
+        Text = text;
+    }
+}
