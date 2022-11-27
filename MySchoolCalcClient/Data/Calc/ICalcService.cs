0b92ee8d078ec7e2a0d027c22ece66a132177c27
VitaliyTka <vtkp72@gmail.com>
2022-11-27 22:14:05 +0200

    MSC-21: trigonometry calc Implementation

--- MySchoolCalcClient/Data/Calc/ICalcService.cs
+++ MySchoolCalcClient/Data/Calc/ICalcService.cs
@@ -3,4 +3,8 @@
 public interface ICalcService
 {
     Task<CalcResponse> GetMathCalc(CalcRequest request);
+    Task<CalcResponse> GetSinCalc(CalcRequest request);
+    Task<CalcResponse> GetCosCalc(CalcRequest request);
+    Task<CalcResponse> GetTanCalc(CalcRequest request);
+    Task<CalcResponse> GetCotCalc(CalcRequest request);
 }
