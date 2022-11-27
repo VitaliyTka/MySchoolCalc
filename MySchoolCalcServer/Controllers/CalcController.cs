7e712bc38ea4bfe0a8e64fd136767074ce232254
VitaliyTka <36275752+VitaliyTka@users.noreply.github.com>
2022-11-27 21:21:20 +0200

    MSC-20: add trigonometry calc api for base operations

--- MySchoolCalcServer/Controllers/CalcController.cs
+++ MySchoolCalcServer/Controllers/CalcController.cs
@@ -16,6 +16,178 @@ namespace MySchoolCalcServer.Controllers
             _logger = logger;
         }
 
+        [HttpGet("sin/{request}")]
+        public CalcResponse GetSin(string request)
+        {
+            try
+            {
+                int degree = Int32.Parse(request);
+                if (degree < 0 || degree > 360)
+                    throw new Exception();
+
+                if (degree == 0 || degree == 180 || degree == 360)
+                    return new CalcResponse("0.0000");
+                if (degree == 90 || degree == 270)
+                    return new CalcResponse("1.0000");
+
+                double radian = CalcRadian(degree);
+                string response = Math.Sin(radian).ToString("0.0000");
+
+                if (degree == 30 || degree == 150)
+                    return new CalcResponse("0.5000");
+                if (degree == 45 || degree == 135)
+                    return new CalcResponse($"√2/2 = {response}");
+                if (degree == 60 || degree == 120)
+                    return new CalcResponse($"√3/2 = {response}");
+
+                if (degree == 210 || degree == 330)
+                    return new CalcResponse("-0.5000");
+                if (degree == 225 || degree == 315)
+                    return new CalcResponse($"-√2/2 = {response}");
+                if (degree == 240 || degree == 300)
+                    return new CalcResponse($"-√3/2 = {response}");
+
+
+                return new CalcResponse(response);
+            }
+            catch (Exception ex)
+            {
+                return new CalcResponse(":(");
+            }
+        }
+
+        [HttpGet("cos/{request}")]
+        public CalcResponse GetCos(string request)
+        {
+            try
+            {
+                int degree = Int32.Parse(request);
+                if (degree < 0 || degree > 360)
+                    throw new Exception();
+
+                if (degree == 0 || degree == 360)
+                    return new CalcResponse("1.0000");
+                if (degree == 90 || degree == 270)
+                    return new CalcResponse("0.0000");
+                if (degree == 180)
+                    return new CalcResponse("-1.0000");
+
+                double radian = CalcRadian(degree);
+                string response = Math.Cos(radian).ToString("0.0000");
+
+                if (degree == 60 || degree == 300)
+                    return new CalcResponse("0.5000");
+                if (degree == 45 || degree == 315)
+                    return new CalcResponse($"√2/2 = {response}");
+                if (degree == 30 || degree == 330)
+                    return new CalcResponse($"√3/2 = {response}");
+
+                if (degree == 120 || degree == 240)
+                    return new CalcResponse("-0.5000");
+                if (degree == 135 || degree == 225)
+                    return new CalcResponse($"-√2/2 = {response}");
+                if (degree == 150 || degree == 210)
+                    return new CalcResponse($"-√3/2 = {response}");
+
+                return new CalcResponse(response);
+            }
+            catch (Exception ex)
+            {
+                return new CalcResponse(":(");
+            }
+        }
+
+        [HttpGet("tan/{request}")]
+        public CalcResponse GetTan(string request)
+        {
+            try
+            {
+                int degree = Int32.Parse(request);
+                if (degree < 0 || degree > 360 || degree == 90 || degree == 270)
+                    throw new Exception();
+
+                if (degree == 90 || degree == 270)
+                    return new CalcResponse("∞");
+
+                if (degree == 45 || degree == 225)
+                    return new CalcResponse("1.0000");
+                if (degree == 0 || degree == 180 || degree == 360)
+                    return new CalcResponse("0.0000");
+                if (degree == 135 || degree == 315)
+                    return new CalcResponse("-1.0000");
+
+                double radian = CalcRadian(degree);
+                string response = Math.Tan(radian).ToString("0.0000");
+
+                if (degree == 60)
+                    return new CalcResponse($"√3 = {response}");
+                if (degree == 30)
+                    return new CalcResponse($"√3/2 = {response}");
+                if (degree == 210)
+                    return new CalcResponse($"√3/3 = {response}");
+
+                if (degree == 120 || degree == 240 || degree == 300)
+                    return new CalcResponse($"-√3 = {response}");
+                if (degree == 150 || degree == 330)
+                    return new CalcResponse($"-√3/3 = {response}");
+
+                return new CalcResponse(response);
+            }
+            catch (Exception ex)
+            {
+                return new CalcResponse(":(");
+            }
+        }
+
+        [HttpGet("cot/{request}")]
+        public CalcResponse GetCot(string request)
+        {
+            try
+            {
+                int degree = Int32.Parse(request);
+                if (degree < 0 || degree > 360)
+                    throw new Exception();
+
+                if (degree == 0 || degree == 180 || degree == 360)
+                    return new CalcResponse("∞");
+
+                if (degree == 45 || degree == 225)
+                    return new CalcResponse("1.0000");
+                if (degree == 90 || degree == 270)
+                    return new CalcResponse("0.0000");
+                if (degree == 135 || degree == 315)
+                    return new CalcResponse("-1.0000");
+
+                double radian = CalcRadian(degree);
+                string response = (1 / Math.Tan(radian)).ToString("0.0000");
+
+                if (degree == 30 || degree == 210)
+                    return new CalcResponse($"√3 = {response}");
+                if (degree == 60)
+                    return new CalcResponse($"√3/2 = {response}");
+                if (degree == 240)
+                    return new CalcResponse($"√3/3 = {response}");
+
+                if (degree == 150 || degree == 330)
+                    return new CalcResponse($"-√3 = {response}");
+                if (degree == 120)
+                    return new CalcResponse($"-√3/2 = {response}");
+                if (degree == 300)
+                    return new CalcResponse($"-√3/3 = {response}");
+
+                return new CalcResponse(response);
+            }
+            catch (Exception ex)
+            {
+                return new CalcResponse(":(");
+            }
+        }
+
+        private double CalcRadian(int degree)
+        {
+            return degree * Math.PI / 180;
+        }
+
         [HttpGet("math/{request}")]
         public CalcResponse Get(string request)
         {
